95c271da94b5899cbb6166bcdada46cd6fd21206
Caleb Sylvest <caleb.sylvest@gmail.com>
2018-10-14 17:12:08 -0500

    fix template typo

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const path = require("path")
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
 
-  const blogPostTemplate = path.resolve(`src/templates/blogPostTemplate.js`)
+  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
 
   return graphql(`
     {
