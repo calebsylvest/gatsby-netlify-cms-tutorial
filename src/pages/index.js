435f6dba3ab6a71c18e112e83cd68614ef78eada
Caleb Sylvest <caleb.sylvest@gmail.com>
2018-10-14 20:44:26 -0500

    list blog posts on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,43 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default ({data}) => {
+  console.log(data)
+  return (
+    <div>
+      <h1>Hello worldzz!</h1>
+      <h2>{data.allMarkdownRemark.totalCount} Posts</h2>
+
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link
+            to={node.frontmatter.path}>
+            <h3>{node.frontmatter.title}</h3>
+          </Link>
+          <span>{node.frontmatter.date}</span>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+
+    </div>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            path
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
